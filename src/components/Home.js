33a54738d3bc1bf03c5eff1d58e83fbc4058c5ab
Abin oommen <oommenabin2gmail.com>
2025-12-17 04:54:06 -0500

    Navbar: add react-scroll, spy, active highlighting

---
+++ src/components/Home.js
@@ -0,0 +1,204 @@
+import React from 'react'
+import { Element } from "react-scroll";
+import Navbar from './NavBar'
+const stats = [
+  { id: 1, name: 'Messages every 24 hours', value: '44 million' },
+  { id: 2, name: 'Assets under holding', value: '$119 trillion' },
+  { id: 3, name: 'New users annually', value: '4M+' },
+]
+const people = [
+  {
+    name: 'Leslie Alexander',
+    role: 'Co-Founder / CEO',
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Michael Foster',
+    role: 'Co-Founder / CTO',
+    imageUrl:
+      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Dries Vincent',
+    role: 'Business Relations',
+    imageUrl:
+      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Lindsay Walton',
+    role: 'Front-end Developer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Courtney Henry',
+    role: 'Designer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Tom Cook',
+    role: 'Director of Product',
+    imageUrl:
+      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+]
+const callouts = [
+  {
+    name: 'Easy chatting',
+    description: 'Simple and user-friendly interface',
+    imageSrc: "/P1.png",
+    imageAlt: 'Easy chatting'
+  },
+  {
+    name: 'Bot',
+    description: 'Bots to assist you',
+    imageSrc: "/Bot.jpg",
+    imageAlt: 'Bots to assist you'
+  },
+  {
+    name: 'Ecommerce',
+    description: 'Daily essentials',
+    imageSrc: 'https://fatturapro.click/wp-content/uploads/2022/09/Vendere-online-senza-partita-iva-.jpg',
+    imageAlt: 'Daily essentials'
+  },
+]
+function Home() {
+  return (
+    <div>
+      <Navbar />
+       
+        <div className="bg-gray-900 min-h-screen">
+        <Element name="home">
+        <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
+        <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+          <svg
+            viewBox="0 0 1024 1024"
+            aria-hidden="true"
+            className="absolute top-1/2 left-1/2 -z-10 size-256 -translate-y-1/2 mask-[radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
+          >
+            <circle r={512} cx={512} cy={512} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
+            <defs>
+              <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+                <stop stopColor="#7775D6" />
+                <stop offset={1} stopColor="#E935C1" />
+              </radialGradient>
+            </defs>
+          </svg>
+          <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+            
+            <h2 className="text-3xl font-semibold tracking-tight text-balance text-white sm:text-4xl">
+              "Connect instantly, chat freely, and share moments effortlessly with our user-friendly chatting app."
+            </h2>
+           
+            <p className="mt-6 text-lg/8 text-pretty text-gray-300">
+              Stay close to your friends and family with seamless messaging anytime, anywhere.
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+              <a
+                href="/signup"
+                className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-xs hover:bg-gray-100 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+              >
+                {' '}
+                Sign up{' '}
+              </a>
+           
+      
+              <a href="/signin" className="text-sm/6 font-semibold text-white hover:text-gray-100">
+                Log in
+                <span aria-hidden="true">→</span>
+              </a>
+            </div>
+          </div>
+          <div className="relative mt-16 h-80 lg:mt-8">
+            <img
+              alt="App screenshot"
+              src="/appui.png"
+              width= 'auto'
+              height= 'auto'
+              className="absolute top-0 left-0 w-228 max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
+            />
+          </div>
+        </div>
+      </div></Element>
+    </div>
+    <div className="bg-white py-24 sm:py-32">
+      
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+          {stats.map((stat) => (
+            <div key={stat.id} className="mx-auto flex max-w-xs flex-col gap-y-4">
+              <dt className="text-base/7 text-gray-600">{stat.name}</dt>
+              <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+                {stat.value}
+              </dd>
+            </div>
+          ))}
+        </dl>
+      </div>
+    </div>
+     <div className="bg-gray-900 py-24 sm:py-32">
+      <div className="mx-auto grid max-w-7xl gap-20 px-6 lg:px-8 xl:grid-cols-3">
+        <div className="max-w-xl">
+          <Element name="team">
+          <h2 className="text-3xl font-semibold tracking-tight text-pretty text-white sm:text-4xl">
+            Meet our leadership
+          </h2></Element>
+          <p className="mt-6 text-lg/8 text-gray-400">
+            We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the
+            best results for our Users.
+          </p>
+        </div>
+        <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
+          {people.map((person) => (
+            <li key={person.name}>
+              <div className="flex items-center gap-x-6">
+                <img
+                  alt=""
+                  src={person.imageUrl}
+                  className="size-16 rounded-full outline-1 -outline-offset-1 outline-white/10"
+                />
+                <div>
+                  <h3 className="text-base/7 font-semibold tracking-tight text-white">{person.name}</h3>
+                  <p className="text-sm/6 font-semibold text-indigo-400">{person.role}</p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+    <div className="bg-gray-100">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
+          <Element name="projects">
+          <h2 className="text-2xl font-bold text-gray-900">PROJECTS</h2>
+          </Element>
+          <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:space-y-0 lg:gap-x-6">
+            {callouts.map((callout) => (
+              <div key={callout.name} className="group relative">
+                <img
+                  alt={callout.imageAlt}
+                  src={callout.imageSrc}
+                  className="w-full rounded-lg bg-white object-cover group-hover:opacity-75 max-sm:h-80 sm:aspect-2/1 lg:aspect-square"
+                />
+                <h3 className="mt-6 text-sm text-gray-500">
+                  <a href={callout.href}>
+                    <span className="absolute inset-0" />
+                    {callout.name}
+                  </a>
+                </h3>
+                <p className="text-base font-semibold text-gray-900">{callout.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+    
+  )
+}
+
+export default Home
